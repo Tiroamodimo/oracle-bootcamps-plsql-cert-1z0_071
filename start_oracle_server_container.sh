5255bd9a7153040a5c6af238b90d2e0fcc04cd07
Tiroamodimo <lot.mothoagae@gmail.com>
2024-04-17 13:12:27 +0200

    code-update: update docker container script

--- start_oracle_server_container.sh
+++ start_oracle_server_container.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
 # Define variables with defaults based on your provided command
-container_name="oracle-user"
+container_name="oracle_bootcamp_db"
 port=1521
 password="oracle123"
-volume_path="/opts/apps/dockerdata/oracle/"
-image_name="gvenzl/oracle-free"
-
-# Read user input for customization (optional)
-read -rp "Enter container name (default: $container_name): " new_container_name
-read -rp "Enter port number (default: $port): " new_port
-read -rp "Enter password for oracle user (default: $password): " new_password
-read -rp "Enter path to local persistent volume (default: $volume_path): " new_volume_path
-read -rp "Enter image name and tag (default: $image_name): " new_image_name
-
-# Update variables with user input if provided
-container_name=${new_container_name:-$container_name}
-port=${new_port:-$port}
-password=${new_password:-$password}
-volume_path=${new_volume_path:-$volume_path}
-image_name=${new_image_name:-$image_name}
+volume_path="/opt/oracle/oradata"
+image_name="gvenzl/oracle-free:latest"
 
 # Construct the docker run command with the configured variables
 docker run -d \
@@ -30,4 +16,10 @@ docker run -d \
   $image_name
 
 echo "Docker container '$container_name' is starting..."
+
+sleep 5
+
+docker exec $container_name resetPassword password123
+
+echo "Oracle sys user password reset"
 
